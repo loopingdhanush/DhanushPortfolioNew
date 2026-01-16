597cb9c25436e805bf01b06bea06dfc8bdd2e25a
dhanush <dhanush.utility@gmail.com>
2026-01-16 12:46:26 +0530

    Unclutering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useEffect, useRef} from 'react'
 import Psg from './assets/img/psg.png'
 import { Badge } from "@/components/ui/badge"
 const skills = ["HTML", "CSS", "JavaScript", "Nodejs", "Expressjs", "React", "Mongodb", "Postgresdb", "Python"]
@@ -27,33 +27,37 @@ import {
 import { Button } from '@/components/ui/button'
 
 const App = () => {
+
+  const containerRef = useRef(null);
+
+ useEffect(() => {
+  const el = containerRef.current;
+  if (!el) return;
+
+  el.classList.remove("fade-in-start");
+
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      el.classList.add("fade-in-start");
+    });
+  });
+}, []);
+
   return (
-  
-<div className="flex flex-col items-center font-sans">
+
+<div ref={containerRef} className="flex flex-col items-center font-sans">
 
   <div className="flex flex-col max-w-6xl p-4 px-6">
 
-    <div className="flex sticky top-0 justify-center sm:justify-start" >
     
-        <NavigationMenu >
-        <NavigationMenuList className=" gap-3 py-1 px-2 rounded-xl w-fill backdrop-blur-md bg-white/20 border-2 mt-6">
-
-          <NavigationMenuItem className="block">
-            <NavigationMenuLink asChild className="bg-transparent font-semibold hover:bg-transparent"><a href='/'>Home</a></NavigationMenuLink>
-          </NavigationMenuItem>
-
-        </NavigationMenuList>
-        </NavigationMenu>
-      
-  </div>
 
     <div className="flex flex-col gap-2 mt-10 justify-center">
       
-      <h1 className="text-3xl md:text-5xl font-semibold " >Hi, I'm Dhanush</h1>
-      <p className="text-md md:text-2xl text-gray-600">
+      <h1 className="text-3xl md:text-5xl font-semibold " data-fade="2" >Hi, I'm Dhanush</h1>
+      <p className="text-md md:text-2xl text-gray-600"  data-fade="3" >
         Undergrad Student at Psg College Of Technology <br></br> Actively open to project collaborations</p>
-        <a className='mt-6 w-8 h-8' href="https://www.linkedin.com/in/loopingdhanush/"> <img src="https://img.icons8.com/ios-glyphs/30/linkedin.png" className='w-full h-full' alt="linkedin"/> </a>
-        <div className="mt-6">
+        <a className='mt-6 w-8 h-8'  data-fade="4"  href="https://www.linkedin.com/in/loopingdhanush/"> <img src="https://img.icons8.com/ios-glyphs/30/linkedin.png" className='w-full h-full' alt="linkedin"/> </a>
+        <div className="mt-6"  data-fade="5" >
           <h2 className="text-xl font-semibold">About</h2>
           <p className="text-md md:text-lg text-gray-600 mt-6">
           I’m a motivated frontend developer driven by curiosity and a passion for learning new web technologies. I enjoy experimenting with tools and frameworks, value feedback for continuous  improvement, and focus on turning ideas into user focused, real world projects.
@@ -61,20 +65,20 @@ const App = () => {
         </div>
     </div>
 
-    <div className="mt-6">
+    <div className="mt-6"  data-fade="6" >
       <h2 className="text-xl font-semibold">Education</h2>
 
       <div className="flex items-center gap-4  mt-6">
         <img src={Psg} className="h-12"/>
           <div>
               <h3 className="text-[17px] font-semibold">PSG College of Technology</h3>
-              <p className="text-[15px] text-gray-800">Bachelor of Technology - Information Technology (2023 - 2027)</p>
+              <p className="text-[15px] text-gray-800">Bachelor of Technology - Information Technology</p>
           </div>
         </div>
     </div>
 
     
-    <div className="mt-6">
+    <div className="mt-6"  data-fade="7" >
       <h2 className="text-xl font-semibold">Skills</h2>
       <ul className="flex flex-wrap gap-2 mt-6">
         
@@ -85,7 +89,7 @@ const App = () => {
       </ul>
     </div>
 
-    <div className="mt-6">
+    <div className="mt-6"  data-fade="8" > 
 
       <h2 className="text-xl font-semibold mb-6">Projects</h2>
       <div className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
